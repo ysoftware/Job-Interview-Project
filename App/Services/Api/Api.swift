d641ef18647c7f7802ed7ed6f7bba112697ad385
Yaroslav Erohin <ysoftware@users.noreply.github.com>
2019-06-23 17:29:28 +0300

    switch to real api

--- App/Services/Api/Api.swift
+++ App/Services/Api/Api.swift
@@ -52,8 +52,8 @@ class Api: ApiProtocol {
 
 	func getRecipes(page:Int,
 					completion: @escaping (Result<ListResponse, Error>) -> Void) {
-		let url = "http://ysoftware.ru/food/search\(page).json"
-//		let url = "https://www.food2fork.com/api/search?key=\(apiKey)&page=\(page)"
+//		let url = "http://ysoftware.ru/food/search\(page).json"
+		let url = "https://www.food2fork.com/api/search?key=\(apiKey)&page=\(page)"
 
 		get(url) { (result:Result<ListResponse, Error>) in
 
@@ -72,8 +72,8 @@ class Api: ApiProtocol {
 
 	func getDetail(id:String,
 				   completion: @escaping (Result<DetailResponse, Error>) -> Void) {
-		let url = "http://ysoftware.ru/food/get\(id).json"
-//		let url = "https://www.food2fork.com/api/get?key=\(apiKey)&rId=\(id)"
+//		let url = "http://ysoftware.ru/food/get\(id).json"
+		let url = "https://www.food2fork.com/api/get?key=\(apiKey)&rId=\(id)"
 
 		get(url) { (result:Result<DetailResponse, Error>) in
 
